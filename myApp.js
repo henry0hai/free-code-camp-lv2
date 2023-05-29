a0fdd795abb46d31135c9520cbc405c401b48f5e
henry <henry0hai@gmail.com>
2023-05-29 14:53:00 +0700

    feat: serve public path

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ let app = express();
 
 console.log("Hello World");
 
+// Serve static files
+app.use('/public', express.static(__dirname + '/public'));
+
+// Route handler
 app.get('/', function(req, res) {
   const absolutePath = __dirname + '/views/index.html'
   res.sendFile(absolutePath);
